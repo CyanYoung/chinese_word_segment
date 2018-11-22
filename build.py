1dd327eb25d4a56e1d465eac7a717a03b7104dcd
yangjc <bupt_yjc@163.com>
2018-11-22 17:28:25 +0800

    change format

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ min_freq = 5
 path_word_vec = 'feat/word_vec.pkl'
 
 
-def word2vec(sents):
+def word2vec(sents, path_word_vec):
     sent_words = [sent.split() for sent in sents]
     model = Word2Vec(sent_words, size=embed_len, window=3, min_count=min_freq, negative=5, iter=10)
     word_vecs = model.wv
@@ -29,7 +29,7 @@ def fit(path_train, path_cpd, train):
     with open(path_train, 'r') as f:
         sents = json.load(f)
     if train:
-        word2vec(sents)
+        word2vec(sents, path_word_vec)
     all_words = ' '.join(sents).split()
     bigrams = list(nltk.ngrams(all_words, 2))
     cfd = nltk.ConditionalFreqDist(bigrams)
